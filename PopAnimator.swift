43320c6e6648cad0f13328c1c4334af0670cc7bf
ElijahButers <Elijahbuters@gmail.com>
2017-05-13 23:42:12 +0300

    Added a new Cocoa Touch Class, a subclass of NSObject, named PopAnimator to the project.

---
+++ PopAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  PopAnimator.swift
+//  BeginnerCook
+//
+//  Created by User on 5/13/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PopAnimator: NSObject {
+
+}
