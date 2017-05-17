5e1df2c4a2b35c2e32c1b51192b0f65ca61cb40e
ElijahButers <Elijahbuters@gmail.com>
2017-05-17 23:25:09 +0300

    Added code to the animateTransition() in the PopAnimator class that adds the view to be presented as a child to the container view and animates transition animations, and wraps up the view controller transition.

--- PopAnimator.swift
+++ PopAnimator.swift
@@ -23,6 +23,15 @@ class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         
         let container = transitionContext.containerView
         let toView = transitionContext.view(forKey: UITransitionContextViewKey.to)!
+        
+        container.addSubview(toView)
+        toView.alpha = 0.0
+        
+        UIView.animate(withDuration: duration, animations: {
+            toView.alpha = 1.0
+        }, completion: { _ in
+            transitionContext.completeTransition(true)
+    })
+        
     }
-
 }
